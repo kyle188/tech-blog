f523199fa67e5523fa085e4fd21ef046f9723192
omen <kybar089@gmail.com>
2022-12-10 12:27:48 -0600

    updated models

--- models/Project.js
+++ models/Project.js
@@ -1,86 +1,46 @@
-const {Model, DataTypes} = require("sequelize");
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-class Post extends Model{
+class Project extends Model {}
 
-}
-Post.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        content: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "user",
-                key: "id"
-            }
-        }
+Project.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: true,
-        updatedAt: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "post"        
-    } 
-)
-module.exports = Post
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    needed_funding: {
+      type: DataTypes.FLOAT,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
 
-
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
-
-// class Project extends Model {}
-
-// Project.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     description: {
-//       type: DataTypes.STRING,
-//     },
-//     date_created: {
-//       type: DataTypes.DATE,
-//       allowNull: false,
-//       defaultValue: DataTypes.NOW,
-//     },
-//     user_id: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: 'user',
-//         key: 'id',
-//       },
-//     },
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'project',
-//   }
-// );
-
-// module.exports = Project;
+module.exports = Project;
